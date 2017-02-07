fcae0255adf635e4ccbbd20fbaca7603eb36187d
axiaoxin <254606826@qq.com>
2017-02-07 20:18:41 +0800

    组合起来

--- guestbook/src/guestbook/routes/home.clj
+++ guestbook/src/guestbook/routes/home.clj
@@ -1,17 +1,22 @@
 (ns guestbook.routes.home
   (:require [compojure.core :refer :all]
             [guestbook.views.layout :as layout]
-            [hiccup.form :refer :all]))
+            [hiccup.form :refer :all]
+            [guestbook.models.db :as db]))
+
+(defn format-time [timestamp]
+  (-> "dd/MM/yyyy"
+      (java.text.SimpleDateFormat.)
+      (.format timestamp)))
 
 (defn show-guests []
   [:ul.guests
    (for [{:keys [message name timestamp]}
-         [{:message "Howdy" :name "Bob" :timestamp nil}
-          {:message "Hello" :name "Bob" :timestamp nil}]]
+         (db/read-guests)]
      [:li
       [:blockquote message]
       [:p "-" [:cite name]]
-      [:time timestamp]])])
+      [:time (format-time timestamp)]])])
 
 (defn home [& [name message error]]
   (layout/common [:h1 "Guestbook"]
@@ -33,7 +38,7 @@
     (home name message "Don't you have something to say?")
     :else
     (do
-      (println name message)
+      (db/save-message name message)
       (home))))
 
 (defroutes home-routes
