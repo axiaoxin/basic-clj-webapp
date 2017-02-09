9fad92a661244a6c180af3497ec57186054d6f3c
axiaoxin <254606826@qq.com>
2017-02-09 20:32:14 +0800

    处理重定向

--- guestbook/src/guestbook/routes/auth.clj
+++ guestbook/src/guestbook/routes/auth.clj
@@ -1,8 +1,9 @@
 (ns guestbook.routes.auth
   (:require [compojure.core :refer [defroutes GET POST]]
             [guestbook.views.layout :as layout]
+            [noir.response :refer [redirect]]
             [hiccup.form :refer
-              [form-to label text-field password-field submit-field]]))
+              [form-to label text-field password-field submit-button]]))
 
 (defn registration-page []
   (layout/common
@@ -17,3 +18,10 @@
              (password-field "pass1")
              [:br]
               (submit-button "create account"))))
+
+(defroutes auth-routes
+  (GET "/register" [] (registration-page))
+  (POST "/register" [id pass pass1]
+        (if (= pass pass1)
+          (redirect "/")
+          (registration-page))))
