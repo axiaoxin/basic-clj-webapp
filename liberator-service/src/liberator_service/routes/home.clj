8988a1c3e5d3536f018be3dd39650568868727bc
axiaoxin <254606826@qq.com>
2017-02-15 21:28:12 +0800

    判定

--- liberator-service/src/liberator_service/routes/home.clj
+++ liberator-service/src/liberator_service/routes/home.clj
@@ -8,5 +8,39 @@
   :etag "fixed-etag"
   :available-media-types ["text/plain"])
 
+(defresource service-available
+  :service-available? false
+  :handle-ok "hello world!"
+  :etag "fixed-etag"
+  :available-media-types ["text/plain"])
+
+(defresource method-allowed
+  :method-allowed?
+  (fn [context]
+    (= :post (get-in context [:request :request-method])))
+  :handle-ok "method allowed"
+  :etag "fixed-etag"
+  :available-media-types ["text/plain"])
+
+(defresource allowed-methods
+  :allowed-methods [:post]
+  :handle-ok "allowed methods"
+  :etag "fixed-etag"
+  :available-media-types ["text/plain"])
+
+(defresource combined
+  :service-available? true
+  :method-allowed? (request-method-in :post)
+  :handle-method-not-allowed
+  (fn [context]
+    (str (get-in context [:request :request-method]) " is not allowed!!!"))
+  :handle-ok "hello world!"
+  :etag "fixed-etag"
+  :available-media-types ["text/plain"])
+
 (defroutes home-routes
-  (ANY "/" request home))
+  (ANY "/" request home)
+  (ANY "/service-available" [] service-available)
+  (ANY "/method-allowed" [] method-allowed)
+  (ANY "/allowed-methods" [] allowed-methods)
+  (ANY "/combined" [] combined))
