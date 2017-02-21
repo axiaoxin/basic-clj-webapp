37483fde2505d25b6535ae63a79d931a658013a3
axiaoxin <254606826@qq.com>
2017-02-21 21:38:08 +0800

    显示报表

--- reporting-example/src/reporting_example/reports.clj
+++ reporting-example/src/reporting_example/reports.clj
@@ -3,27 +3,27 @@
             [reporting-example.models.db :as db]))
 
 ;; make a pdf
-(pdf
-  [{:header "Wow that was easy"}
-   [:list
-    [:chunk {:style :bold} "A bold item"]
-    "another item"
-    "yet another item"]
-   [:paragraph "I'm a paragraph!"]]
-"doc.pdf")
+; (pdf
+;   [{:header "Wow that was easy"}
+;    [:list
+;     [:chunk {:style :bold} "A bold item"]
+;     "another item"
+;     "yet another item"]
+;    [:paragraph "I'm a paragraph!"]]
+; "doc.pdf")
 
 (def employee-template
   (template [$name $occupation $place $country]))
 
 ;; make a pdf by db
-(pdf
-  [{:header "Employee List"}
-    (into [:table
-            {:border false
-             :cell-border false
-             :header [{:color [0 150 150]} "Name" "Occupation" "Place" "Country"]}]
-          (employee-template (db/read-employees)))]
-   "report.pdf")
+; (pdf
+;   [{:header "Employee List"}
+;     (into [:table
+;             {:border false
+;              :cell-border false
+;              :header [{:color [0 150 150]} "Name" "Occupation" "Place" "Country"]}]
+;           (employee-template (db/read-employees)))]
+;    "report.pdf")
 
 (def employee-template-paragraph
   (template
@@ -35,10 +35,29 @@
       [:spacer]]))
 
 ;; make a styled pdf
-(pdf
-  [{}
-   [:heading {:size 10} "Employees"]
-   [:line]
-   [:spacer]
-   (employee-template-paragraph (db/read-employees))]
- "report-style.pdf")
+; (pdf
+;   [{}
+;    [:heading {:size 10} "Employees"]
+;    [:line]
+;    [:spacer]
+;    (employee-template-paragraph (db/read-employees))]
+;  "report-style.pdf")
+
+(defn table-report [out]
+  (pdf
+    [{:header "Employee List"}
+     (into [:table
+            {:border false
+             :cell-border false
+             :header [{:color [0 150 150]} "Name" "Occupation" "Place" "Country"]}]
+      (employee-template (db/read-employees)))]
+   out))
+
+(defn list-report [out]
+  (pdf
+    [{}
+     [:heading {:size 10} "Employees"]
+     [:line]
+     [:spacer]
+    (employee-template-paragraph (db/read-employees))]
+   out))
